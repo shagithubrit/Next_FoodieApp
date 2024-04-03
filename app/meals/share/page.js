664c4d86daf77dfc0a61426d2ebb48a209f679a8
Shashwat Kapoor <shashwatquora2000@gmail.com>
2024-04-04 05:14:25 +0530

    root header file created

---
+++ app/meals/share/page.js
@@ -0,0 +1,3 @@
+export default function shareMealPage() {
+  return <h1>Share Meal Page</h1>;
+}
---
+++ components/main-header.js
@@ -0,0 +1,24 @@
+const { default: Link } = require("next/link");
+
+// this header should be shared among all the page
+import logoImg from "@/assets/logo.png";
+export default function MainHeader() {
+  return (
+    <header>
+      <Link href="/">
+        <img src={logoImg.src} alt="A plate  with food on it" />
+        Next Level Food
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community"> Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
