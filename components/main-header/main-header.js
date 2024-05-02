c40977bac32fcfd43d34927685f49f22e7209b69
Shashwat Kapoor <shashwatquora2000@gmail.com>
2024-05-02 08:41:55 +0530

    structure changed

---
+++ components/main-header/main-header.js
@@ -0,0 +1,29 @@
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+const { default: Link } = require("next/link");
+import Image from "next/image";
+// this header should be shared among all the page
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate  with food on it" priority />
+          Next Level Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community"> Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
